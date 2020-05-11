22ae4b3d352c30b72598684b07b18739dc8f08da
Raisa Tarasova <raisatarasova@MacBook-Pro-Raisa.local>
2020-05-11 23:05:09 +0300

    change

--- src/test/scala/simulations/RuntimeParameters.scala
+++ src/test/scala/simulations/RuntimeParameters.scala
@@ -2,15 +2,14 @@ package simulations
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
 import scala.concurrent.duration._
 
-class RuntimeParameters extends Simulation {
+class RunTimeParameters extends Simulation{
 
   private def getProperty(propertyName: String, defaultValue: String) = {
     Option(System.getenv(propertyName))
-      .orElse(Option(System.getProperty(propertyName)))
-      .getOrElse(defaultValue)
+    .orElse(Option(System.getProperty(propertyName)))
+    .getOrElse(defaultValue)
   }
 
   def userCount: Int = getProperty("USERS", "5").toInt
@@ -20,31 +19,31 @@ class RuntimeParameters extends Simulation {
   before {
     println(s"Running test with ${userCount} users")
     println(s"Ramping users over ${rampDuration} seconds")
-    println(s"Total test duration: ${testDuration} seconds")
+    println(s"Total test duration ${testDuration} seconds")
   }
 
-  val httpConf = http.baseUrl("http://localhost:8080/app/")
+  val httpConf = http.baseUrl("http://video-game-db.eu-west-2.elasticbeanstalk.com/app/")
     .header("Accept", "application/json")
 
   def getAllVideoGames() = {
     exec(
-      http("Get all video games")
+      http("Get all videogames")
         .get("videogames")
         .check(status.is(200))
     )
   }
 
-  val scn = scenario("Get all video games")
+  val scn = scenario("Get all videogames")
     .forever() {
       exec(getAllVideoGames())
     }
 
+
   setUp(
     scn.inject(
       nothingFor(5 seconds),
-      rampUsers(userCount) during (rampDuration second)
-    )
-  ).protocols(httpConf)
-    .maxDuration(testDuration seconds)
+      rampUsers(userCount) during (rampDuration seconds)
+    ).protocols(httpConf)
+  ).maxDuration(testDuration seconds)
 
 }
